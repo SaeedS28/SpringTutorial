a8a45892c987819aefaac9a41f761a08e405474b
Saeeds28 <awesomesaad.saeed2@gmail.com>
2019-12-01 16:54:21 -0500

    introduced ApplicationContext.xml file for bean injection

--- SpringTutorial/src/main/java/com/saeeds28/SpringTutorial/main/App.java
+++ SpringTutorial/src/main/java/com/saeeds28/SpringTutorial/main/App.java
@@ -1,13 +1,20 @@
 package com.saeeds28.SpringTutorial.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.saeeds28.SpringTutorial.interfaces.Vehicle;
+import com.saeeds28.SpringTutorial.pojos.Car;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App  {
+	
+    public static void main( String[] args ) {
+        ApplicationContext con = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Vehicle obj = con.getBean(Car.class);
+        obj.drive();
     }
 }
